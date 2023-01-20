7d014b6380a1d2797dbcd569ee5bd72a2ed83df5
SEBXR <99262672+SEBXR@users.noreply.github.com>
2023-01-20 15:21:54 +0300

    Ajout de dépendance de mongoDB

--- src/main/java/springapp/Main.java
+++ src/main/java/springapp/Main.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Client c = new Client("Andriantsiory", "Mandresy", "123456", "mandresy@gmail.com", 2500000, 0);
-        System.out.println(c.Inscrisption());
+        
     }
 }
